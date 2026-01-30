395c75b88c4236b8dfdcccaf4eb0da8943a12204
ilya <ilifud98@mail.ru>
2026-01-30 20:57:18 +0700

    refactor(./models) add all models app

--- client/models.py
+++ client/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+from user.models import CustomUser
+
+
+class Subscriber(models.Model):
+    """Модель «Получатель рассылки»"""
+
+    full_name = models.CharField(max_length=255, verbose_name="Ф.И.О.")
+    email = models.EmailField(unique=True, verbose_name="Почта")
+    comment = models.TextField(blank=True, verbose_name="Комментарий")
+    owner = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="subscriber_owner", default=1)
+
+    def __str__(self):
+        return self.full_name
--- message/models.py
+++ message/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Message(models.Model):
+    """Модель «Сообщение»"""
+
+    subject = models.CharField(max_length=255, verbose_name="Тема письма")
+    body = models.TextField(null=True, blank=True, verbose_name="Тело письма")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата и время создания")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата и время последнего обновления")
+
+    def __str__(self):
+        return self.subject

a496bf4e821f140c3d78927df0184677f629565c
ilya <ilifud98@mail.ru>
2026-01-31 17:37:09 +0700

    refactor(config.urls) add new path message, mailings, client, user, home

--- config/urls.py
+++ config/urls.py
@@ -14,10 +14,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from mailings.views import CampaignView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("client/", include(('client.urls', 'client'), namespace='client')),
+    path("mailings/", include(('mailings.urls', 'mailings'), namespace='mailings')),
+    path("message/", include(('message.urls', 'message'), namespace='message')),
+    path("user/", include(('user.urls', 'user'), namespace='user')),
+    path('', CampaignView.as_view(), name='home')
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
